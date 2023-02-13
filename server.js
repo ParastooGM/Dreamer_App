6f17a70250988226c72e0f564bb84fd431af7113
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2023-02-13 15:39:29 -0500

    fix mongo connection.

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 const db = process.env.MONGO_URI;
 
 mongoose
-    .connect(db)
-    .then(() => {
-        console.log("MongoDB connected");
-        const port = process.env.PORT || 5000;
-        app.listen(port, () => console.log("served started"));
-    })
-    .catch((err) => console.log(err));
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected");
 
-app.use("/api/items", require("./routes/api/items"));
-app.use("/api/users", require("./routes/api/users"));
+    app.use("/api/items", require("./routes/api/items"));
+    app.use("/api/users", require("./routes/api/users"));
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => console.log("served started"));
+  })
+  .catch((err) => console.log(err));
