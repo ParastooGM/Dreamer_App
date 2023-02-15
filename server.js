704d2603698a3184d04811fc4f8963dbd0ebadb1
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2023-02-15 11:42:59 -0500

    update server

--- server.js
+++ server.js
@@ -11,13 +11,20 @@ const db = process.env.MONGO_URI;
 
 mongoose
   .connect(db)
-  .then(() => {
-    console.log("MongoDB connected");
-
-    app.use("/api/items", require("./routes/api/items"));
-    app.use("/api/users", require("./routes/api/users"));
-
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => console.log("served started"));
-  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("served started"));
