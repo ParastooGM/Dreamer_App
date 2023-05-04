8810e89115a496696d8155f76b587e60159339a1
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2023-05-04 17:09:34 -0400

    Fix bugs.

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("served started"));
+app.listen(port, () => console.log("server started"));
